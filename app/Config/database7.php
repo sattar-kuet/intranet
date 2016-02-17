82ce0de4da643320aaddd1d22db464cf13a8db96
Md. Omar Faruk <farukmscse@gmail.com>
2016-02-17 15:07:21 +0600

    files ready

--- app/Config/database7.php
+++ app/Config/database7.php
@@ -74,7 +74,7 @@ class DATABASE_CONFIG {
 		'host' => 'localhost',
 		'login' => 'root',
 		'password' => '',
-		'database' => 'totalcable',
+		'database' => 'totalcableusa',
 		'prefix' => '',
 		//'encoding' => 'utf8',
 	);
