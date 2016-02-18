a21647a00c29087d1c710538dfd339540e16bf7c
Abdus Sattar <sattar.kuet@gmail.com>
2016-02-18 19:24:45 +0600

    resolve conflict

--- app/Controller/TicketsController.php
+++ app/Controller/TicketsController.php
@@ -89,6 +89,8 @@ class TicketsController extends AppController {
     function unsolve() {
         $this->loadModel('Track');
         $this->request->data['Track']['status'] = 'unresolved';
+         $loggedUser = $this->Auth->user();
+        $this->request->data['Track']['forwarded_by'] = $loggedUser['id'];
         $this->Track->save($this->request->data['Track']);
         $msg = '<div class="alert alert-warning">
  <button type="button" class="close" data-dismiss="alert">&times;</button>
