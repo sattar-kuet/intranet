7c6fe9f5183edae6a19c8ccbab0e7e9bb2256f5a
Md. Omar Faruk <farukmscse@gmail.com>
2016-03-03 20:33:04 +0600

    create new session

--- app/webroot/js/serviceManageDrodown.validation.js
+++ app/webroot/js/serviceManageDrodown.validation.js
@@ -5,7 +5,7 @@ $(function () {
         var ddlist = $("#ddlist");
         if (ddlist.val() == "") {
             //If the "Please Select" option is selected display error.
-            alert("Please select a list data!");
+            alert("Please select a dropdown data!");
             return false;
         }
         return true;
