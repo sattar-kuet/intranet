0a79dea94c3e501c2628acdcdf658d3b16ddfd2b
abdullah <abdullah88miah@gmail.com>
2016-04-03 22:50:48 -0700

    get update

--- app/webroot/js/sum.js
+++ app/webroot/js/sum.js
@@ -142,3 +142,30 @@ function printDiv(printableArea) {
     document.body.innerHTML = originalContents;
 }
 
+//  start show & hide shipment in customer rezistration 
+$(document).ready(function (){
+   $('#shipment').click(function (){
+       if($(this).is(":checked")){
+           $('#shipmentshow_hide').show();
+       }
+        else{
+           $('#shipmentshow_hide').hide();  
+        }
+   });
+});
+//  end show & hide shipment in customer rezistration
+
+// start Additional info 
+$(document).ready(function (){
+    $('#additioninfo').click(function (){
+       if($(this).is(":checked")){
+           $('#Additional_info').show();
+       }
+        else{
+           $('#Additional_info').hide();  
+        } 
+    });
+    
+});
+
+// end Additional info 
