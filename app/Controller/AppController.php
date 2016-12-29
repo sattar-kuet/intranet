ebb693337e36dcab9c657f8ec17291ac7cef4661
Md. Omar Faruk <farukmscse@gmail.com>
2016-12-29 14:18:10 +0600

    update files

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -52,7 +52,7 @@ class AppController extends Controller {
             )
         )
     );
-    public $per_page = 1;
+    public $per_page = 200;
 
     public function beforeFilter() {
         // save last visited url
